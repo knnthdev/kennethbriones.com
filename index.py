d7f270012476376a11eb0624c69b316a3be79ab6
lenovo <lenovo@DESKTOP-3BOMN4U>
2021-09-19 17:52:12 -0600

    save-0001

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
 
+
+@app.route("/About")
+def about():
+    return render_template("about.html")
+
+ 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5500)
+    print("the program is already")
+ 
