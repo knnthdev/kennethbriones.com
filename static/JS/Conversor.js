b69ad1cc45235b55755fb1181ee80c9f4fc00718
lenovo <lenovo@DESKTOP-3BOMN4U>
2021-09-19 18:31:18 -0600

    git-1001

---
+++ static/JS/Conversor.js
@@ -0,0 +1,19 @@
+class Convert
+{
+    constructor()
+    {
+
+    }
+    static ToInt(key)
+     {
+        switch (key) {
+            case "0":
+                return 0;
+            case "1":
+                return 1;
+
+            default:
+                break;
+        }
+     }
+}
